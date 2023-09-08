6ab21a586ecb2a2816839bfe0e3d54a76cb131e8
VinDazy <hectorvalinci@gmail.com>
2023-09-08 18:21:09 +0100

    error handling when accessing passwords before login

--- pages/2_🔐_My Passwords.py
+++ pages/2_🔐_My Passwords.py
@@ -26,51 +26,52 @@ deta_pass = Deta(DETA_PASS_KEY)
 
 db_pass = deta_pass.Base('password')
 
+try :
+    file_path = "email.pickle"
 
-# Specify the file path where the pickle file is located
-file_path = "email.pickle"
+    try:
+        # Open the pickle file in binary read mode
+        with open(file_path, 'rb', encoding='utf-8') as file:
+            # Load the data from the pickle file
+            email = pickle.load(file)
+            print(f"Read email from pickle file: {email}")
+    except FileNotFoundError:
+        print(f"Pickle file '{file_path}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-try:
-    # Open the pickle file in binary read mode
-    with open(file_path, 'rb', encoding='utf-8') as file:
-        # Load the data from the pickle file
-        email = pickle.load(file)
-        print(f"Read email from pickle file: {email}")
-except FileNotFoundError:
-    print(f"Pickle file '{file_path}' not found.")
-except Exception as e:
-    print(f"An error occurred: {e}")
+    with st.sidebar:
+        st.image('media/welcome.gif')
+        st.success("Displaying Passwords",icon="✅")
+    def fetch_user_passwords(email):
+        user_data_list = []
+        users = db_pass.fetch()
 
-with st.sidebar:
-    st.image('media/welcome.gif')
-    st.success("Displaying Passwords",icon="✅")
-def fetch_user_passwords(email):
-    user_data_list = []
-    users = db_pass.fetch()
+        for user in users.items:
+            if user['User_Email'] == email:
+                new_data = {
+                    'Password_id': user['Password_id'],
+                    'User_Email': user['User_Email'],
+                    'Password': user['Password'],
+                    'Password_Domain': user['Password_Domain'],
+                    'Password_length': user['Password_length']
+                }
+                user_data_list.append(new_data)
 
-    for user in users.items:
-        if user['User_Email'] == email:
-            new_data = {
-                'Password_id': user['Password_id'],
-                'User_Email': user['User_Email'],
-                'Password': user['Password'],
-                'Password_Domain': user['Password_Domain'],
-                'Password_length': user['Password_length']
-            }
-            user_data_list.append(new_data)
-
-    # Create a DataFrame from the list of user data dictionaries
-    df = pd.DataFrame(user_data_list)
-    
-    # Display the data in a Streamlit web app
-    st.write("User Email:", email)
-    
-    if not df.empty:
-        st.write(df)
+        # Create a DataFrame from the list of user data dictionaries
+        df = pd.DataFrame(user_data_list)
         
-    else:
-        st.write("No passwords found for this user.")
+        # Display the data in a Streamlit web app
+        st.write("User Email:", email)
+        
+        if not df.empty:
+            st.write(df)
+            
+        else:
+            st.write("No passwords found for this user.")
 
-fetch_user_passwords(email=email)
+    fetch_user_passwords(email=email)
+except:
+    st.error("Please Log in first")
 
 
